31d9af99a59bdcf50f8d33fe784136645ffc09e4
manana <manana@A13E21>
2024-06-04 09:45:06 +0200

    111

---
+++ 22_operadores_bits/src/Test1.java
@@ -0,0 +1,13 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		int a=2;
+		int b=~a;
+		System.out.println(Integer.toBinaryString(b));
+		System.out.println(Integer.toBinaryString(a));
+		
+
+	}
+
+}
