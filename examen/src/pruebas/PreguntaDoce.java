c953993b3423f2d12337f5f6f14131dea0f052ef
manana <manana@A13E21>
2024-06-06 13:39:12 +0200

    44

--- examen/src/pruebas/PreguntaDoce.java
+++ examen/src/pruebas/PreguntaDoce.java
@@ -1,10 +1,19 @@
 package pruebas;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class PreguntaDoce {
+	
+	public enum Desig {
+		CEO('A'), CMO('B'), CTO('C'), CFO('D');
+		char c;
+		private Desig (char c) {
+			this.c=c;
+		}
+	}
 
 	public static void main(String[] args) {
 		int a=2;
@@ -30,6 +39,14 @@ public class PreguntaDoce {
 		int x3=Collections.binarySearch(lst, "e3");
 		System.out.println(x3);
 		
+		Arrays.stream(Desig.values()).dropWhile (s->s.equals(Desig.CMO));
+		switch (Desig.valueOf("CMO")) {
+			case CEO -> System.out.println("Execute");
+			case CMO -> System.out.println("Marketin");
+			case CFO -> System.out.println("FIU");
+			case CTO-> System.out.println("TEC");
+			default -> System.out.println("def");		
 	}
 
+}
 }
